5b2fc2c9de3b03d8c072e39126a450beef422e4a
nuno929 <t-mark5@kce.biglobe.ne.jp>
2020-05-18 02:19:05 +0900

    feat : テストを簡単にするためのmodule分割

--- storage/storage/storage.js
+++ storage/storage/storage.js
@@ -1,6 +1,4 @@
-const aws = require('aws-sdk')
-aws.config.region = process.env.REGION
-const s3 = new aws.S3()
+const { s3 } = require('./get-s3.js')
 
 const putFile = async (fileKey, body) => {
   const putParams = {
